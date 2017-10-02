43d23023b2b7c851ebda5045db56281c3b2d8931
Nicholas Gilpin <nicholascgilpin123@gmail.com>
2017-10-02 15:18:43 -0500

    Add dummy getData() function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Data
+function getData(config) {
+  return {
+    title: "Thriller",
+    date: "10-2-2017",
+    time: "3:00pm",
+    location: "1350 Texas Dr",
+    routineVideo: "https://youtu.be/dQw4w9WgXcQ",
+    bannerImage: "https://dustn.tv/free-resources/social-covers/facebook-cover-photo-template.jpg",
+    locationImg: "https://dustn.tv/free-resources/social-covers/facebook-cover-photo-template.jpg",
+    sponser: "Oreos",
+    adminID: "o438yt480ht48ty4o8gto84elgh8to4ht8o37ogf",
+    contactEmail: "theflashmobsters@gmail.com",
+    uid: "NOT_LOGGED_IN",
+    peopleIntrested: 247,
+    announcments: [
+      {text:" Don't forget to wear yellow!", adminID:"o438yt480ht48ty4o8gto84elgh8to4ht8o37ogf", date:"10-3-2017"},
+      {text:" Don't forget to wear black!", adminID:"o438yt480ht48ty4o8gto84elgh8to4ht8o37ogf", date:"10-1-2017"},
+      {text:" Don't forget to wear maroon!", adminID:"o438yt480ht48ty4o8gto84elgh8to4ht8o37ogf", date:"10-9-2017"}
+    ]
+  }
+}
+// Preprocessing of data
+console.log(JSON.stringify(getData(1234)))
+
+// Rendering
 class App extends Component {
   render() {
     return (
