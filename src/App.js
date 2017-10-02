1b754e00105439ffdd9fcbf0037eef5c93f7557b
Clayton Petty <git.notyalc@gmail.com>
2017-10-02 15:34:52 -0500

    initial firebase integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import fire from './fire';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { flashmobs: [] }; // <- set up react state
+  }
+  componentWillMount(){
+    /* Create reference to messages in Firebase Database */
+    let flashmobsRef = fire.database().ref('flashmobs').orderByKey().limitToLast(100);
+    flashmobsRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let flashmob = { details: snapshot.val(), id: snapshot.key };
+      this.setState({ flashmobs: [flashmob].concat(this.state.flashmobs) });
+    })
+  }
+  addFlashmob(e){
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    // Construct flashmob object
+    var flashMobInstance =  {
+        'name': this.name.value,
+        'desc': this.desc.value,
+        'loc': this.loc.value
+    };
+    
+    /* Send the message to Firebase */
+    fire.database().ref('flashmobs').push(flashMobInstance);
+    this.inputEl.value = ''; // <- clear the input
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.addFlashmob.bind(this)}>
+        <input type="text" ref={ el => this.name = el }/>
+        <input type="text" ref={ el => this.desc = el }/>
+        <input type="text" ref={ el => this.loc = el }/>
+        <input type="submit"/>
+        <ul>
+          { /* Render the list of messages */
+            this.state.flashmobs.map( flashmob => <li key={flashmob.id}>{flashmob.details.name}</li> )
+          }
+        </ul>
+      </form>
     );
   }
 }
