9dfdbabb5c3883820a25c7f07f2ebf0dd60eb8c3
Aaron Hillier <aaron123@tamu.edu>
2017-10-03 21:54:37 -0500

    Put in database stuff

--- src/App.js
+++ src/App.js
@@ -26,8 +26,25 @@ function getData(config) {
     ]
   }
 }
+
+//get full flashmob item from flashmob id
+function getFlashMob(flashMobId){
+  var flashMobRef = fire.database().ref('/flashmobs/'+flashMobId);
+  flashMobRef.once("value").then(function(snapshot){
+    console.log(snapshot.val());
+    var flashMobSnap = snapshot.val();
+    document.getElementById("flashMobTitle").innerHTML = flashMobSnap.name;
+    document.getElementById("flashMobDate").innerHTML = flashMobSnap.date + flashMobSnap.time;
+    document.getElementById("flashMobName").innerHTML = flashMobSnap.location;
+    document.getElementById("flashMobDescription").innerHtml = flashMobSnap.description;
+    return flashMobSnap;
+  })
+}
+
+
 // Preprocessing of data
-var data = getData(1234)
+var data = getData(1234);
+var thisFlashMob = getFlashMob("-Kv_DgsoprFx0Z4st7Dq");
 
 // Rendering
 class App extends Component {
@@ -44,27 +61,41 @@ class App extends Component {
       this.setState({ flashmobs: [flashmob].concat(this.state.flashmobs) });
     })
   }
+
+
   addFlashmob(e){
     e.preventDefault(); // <- prevent form submit from reloading the page
     // Construct flashmob object
     var flashMobInstance =  {
         'name': this.name.value,
-        'desc': this.desc.value,
-        'loc': this.loc.value
+        'description': this.description.value,
+        'location': this.location.value,
+        'date': this.date.value,
+        'time': this.time.value,
+        'adminID': this.adminID.value,
+        'email': this.email.value
     };
     
     /* Send the message to Firebase */
     fire.database().ref('flashmobs').push(flashMobInstance);
     this.inputEl.value = ''; // <- clear the input
   }
+
+
   render() {
     return (
 	<div>
       <form onSubmit={this.addFlashmob.bind(this)}>
-        <input type="text" ref={ el => this.name = el }/>
-        <input type="text" ref={ el => this.desc = el }/>
-        <input type="text" ref={ el => this.loc = el }/>
+
+        <input type="text" placeholder="name"ref={ el => this.name = el }/>
+        <input type="text" placeholder="description" ref={ el => this.description = el }/>
+        <input type="text" placeholder="location" ref={ el => this.location = el }/>
+        <input type="date" placeholder="date" ref={ el => this.date = el }/>
+        <input type="time" placeholder="time" ref={ el => this.time = el }/>
+        <input type="text" placeholder="adminID" ref= {el => this.adminID = el}/>
+        <input type="email" placeholder="email" ref= {el => this.email = el}/>
         <input type="submit"/>
+
         <ul>
           { /* Render the list of messages */
             this.state.flashmobs.map( flashmob => <li key={flashmob.id}>{flashmob.details.name}</li> )
@@ -73,14 +104,14 @@ class App extends Component {
       </form>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Capone</h1>
-          <span>Login</span>
+          <h1 id="flashMobTitle" className="App-title">Capone</h1>
+          <div id="flashMobDescription">{data.sponser}</div>
         </header>
         <img src={data["bannerImage"]}></img>
-        <h1>{data.title}</h1>
+        <h1 id="flashMobName">{data.title}</h1>
         <div>
-          <div>{data.time + " " + data.date}</div>
-          <div>{data.location}</div>
+          <div id="flashMobDate">{data.time + " " + data.date}</div>
+          <div id="flashMobTime">{data.date}</div>
           <div>
             <iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k">
             </iframe>
