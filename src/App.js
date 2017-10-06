18f68fd69af75cc02fb17e4004a1365324c19bbd
Nicholas Gilpin <nicholascgilpin123@gmail.com>
2017-10-06 10:26:08 -0500

    Converted admin page into a component

--- src/App.js
+++ src/App.js
@@ -93,36 +93,74 @@ function MobPublicView(props){
 }
 // Creates a page where an admin can view and edit mob details
 function MobAdminView(props){
+  /*constructor(props) {
+    super(props);
+    this.state = { flashmobs: [] }; // <- set up react state
+  }*/
+  /*componentWillMount(){*/
+    /* Create reference to messages in Firebase Database */
+    /*let flashmobsRef = fire.database().ref('flashmobs').orderByKey().limitToLast(100);*/
+    /*flashmobsRef.on('child_added', snapshot => {*/
+      /* Update React state when message is added at Firebase Database */
+      /*let flashmob = { details: snapshot.val(), id: snapshot.key };*/
+      /*this.setState({ flashmobs: [flashmob].concat(this.state.flashmobs) });*/
+    /*})*/
+  /*}*/
+  var pageData = {}
+
+  pageData.addFlashmob = function (e){
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    // Construct flashmob object
+    var flashMobInstance =  {
+        'name': this.name.value,
+        'bannerImage': this.bannerImage,
+        'description': this.description.value,
+        'date': this.date.value,
+        'time': this.time.value,
+        'location': this.location.value,
+        'adminID': this.adminID.value,
+        'adminName': this.adminName.value,
+        'adminEmail': this.adminEmail.value,
+        'numInterested': this.numInterested.value,
+        'video': this.video.value,
+        'locImage': this.locImage.value,
+        'announcements': this.announcements
+    };
+
+    /* Send the message to Firebase */
+    fire.database().ref('flashmobs').push(flashMobInstance);
+    this.inputEl.value = ''; // <- clear the input
+  }
   return (
     <div align="center">
-      <form className="App-form" onSubmit={this.addFlashmob.bind(this)}>
+      <form className="App-form" onSubmit={pageData.addFlashmob.bind(pageData)}>
         <div align="center" style={{position: 'relative'}}>
-          <input className="App-input-large" type="text" placeholder="event banner image..." ref={ el => this.bannerImage = el}/>
+          <input className="App-input-large" type="text" placeholder="event banner image..." ref={ el => pageData.bannerImage = el}/>
           <div align="center" style={{position: 'absolute', top: '0', left: '0', right:'0', padding:'100px'}}>
-            <input className="App-input-small" type="text" placeholder="event name..." ref={ el => this.name = el }/>
+            <input className="App-input-small" type="text" placeholder="event name..." ref={ el => pageData.name = el }/>
           </div>
         </div>
         <div align="center">
           <span>
-            <input className="App-input-small" type="text" placeholder="event date and time..." ref={ el => this.date = el }/>
-            <input className="App-input-small" type="text" placeholder="event choreographer..." ref={ el => this.adminEmail = el }/>
+            <input className="App-input-small" type="text" placeholder="event date and time..." ref={ el => pageData.date = el }/>
+            <input className="App-input-small" type="text" placeholder="event choreographer..." ref={ el => pageData.adminEmail = el }/>
           </span>
           <span>
-            <input className="App-input-small" type="text" placeholder="event location..." ref={ el => this.loc = el }/>
-            <input className="App-input-small" type="text" placeholder="event choreographer email..." ref={ el => this.adminID = el }/>
+            <input className="App-input-small" type="text" placeholder="event location..." ref={ el => pageData.loc = el }/>
+            <input className="App-input-small" type="text" placeholder="event choreographer email..." ref={ el => pageData.adminID = el }/>
           </span>
         </div>
         <div align="center">
           <span>
-            <input className="App-input-small" type="text" placeholder="maximum numer of people..." ref={ el => this.maxPeople = el }/>
+            <input className="App-input-small" type="text" placeholder="maximum numer of people..." ref={ el => pageData.maxPeople = el }/>
             <button className="button" vertical-align="middle"><span> I am interested! </span></button>
           </span>
         </div>
         <div align="center">
-          <input className="App-input-large" type="text" placeholder="dance choreography video..." ref={ el => this.video = el }/>
+          <input className="App-input-large" type="text" placeholder="dance choreography video..." ref={ el => pageData.video = el }/>
         </div>
         <div align="center">
-          <input className="App-input-large" type="text" placeholder="event location image..." ref={ el => this.locImg = el }/>
+          <input className="App-input-large" type="text" placeholder="event location image..." ref={ el => pageData.locImg = el }/>
         </div>
         <div align="center">
           <input className="App-input" type="submit"/>
