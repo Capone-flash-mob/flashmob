9572bebdb5f3119289a4ee2ddad8b710b96c7283
Clayton Petty <git.notyalc@gmail.com>
2017-10-31 02:21:01 -0500

    more home page css tweeks

--- src/HomeView.js
+++ src/HomeView.js
@@ -44,25 +44,28 @@ var HomeView = class HomeView extends Component {
     console.log("YAYAYAYA " + flashList[0].key);
     return(
       <div class="content">
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-8">
-          {flashList.map((key) =>
-            <div class="col-sm-6">
-              <div class="mob-infobox">
-                <YouTube
-                  opts={opts}
-                  videoId="D59v74k5flU"
-                />
-                <div class="mob-infobox-details">
-                  <span class="mob-infobox-title"> {key.name} </span>
-                  <span class="mob-infobox-location"> {key.location} </span>
-                  <span class="mob-infobox-date-and-time"> {key.date}, {key.time} </span>
-                  <Link to={"mob/" + key.key}>Click to visit</Link>
-                </div>
+        <div class="row">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-8">
+            {flashList.map((key) =>
+              <div class="col-sm-6">
+                <Link to={"mob/" + key.key} style={{ textDecoration: 'none'}}>
+                  <div class="mob-infobox">
+                    <YouTube
+                      opts={opts}
+                      videoId="D59v74k5flU"
+                    />
+                    <div class="mob-infobox-details">
+                      <span class="mob-infobox-title"> {key.name} </span>
+                      <span class="mob-infobox-location"> {key.location} </span>
+                      <span class="mob-infobox-date-and-time"> {key.date}, {key.time} </span>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
         <div class="col-sm-2">
         </div>
