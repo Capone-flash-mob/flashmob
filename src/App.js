f8a491a07e9151097c788aa05ade453a2aa90d41
Aaron Hillier <aaron123@tamu.edu>
2017-10-31 21:17:04 -0500

    Redid functions

--- src/App.js
+++ src/App.js
@@ -122,7 +122,39 @@ class Headline extends React.Component{
   }*/
 
   render() {
+        if (this.state.authenticated == 'true') {
+         var user = firebase.auth().currentUser;
+
     return (
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary navbar-container">
+        <Link class="navbar-brand navbar-title" to="/">CAPONE</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse-elements" aria-controls="navbar-collapse-elements" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-beginning navbar-collapse-elements" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <div class="navbar-link-container">
+              <Link class="nav-item nav-link navbar-link" to="/Create">Create a flashmob</Link>
+            </div>
+            <div class="navbar-link-container">
+              <Link class="nav-item nav-link navbar-link" to="/about">About</Link>
+            </div>
+          </div>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end navbar-collapse-elements" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <div class="navbar-link-container">
+              <a class="nav-item nav-link navbar-link" onClick={this.signOut}>Sign Out</a>
+            </div>
+            <div class="navbar-link-container">
+              <Link to={"/user/" + user.uid} style={{textDecoration: 'none'}} ><a class="nav-item nav-link navbar-link mr-auto" >{user.displayName}</a></Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )}
+    else {
+      return (
       <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary navbar-container">
         <Link class="navbar-brand navbar-title" to="/">CAPONE</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse-elements" aria-controls="navbar-collapse-elements" aria-expanded="false" aria-label="Toggle navigation">
@@ -143,13 +175,13 @@ class Headline extends React.Component{
             <div class="navbar-link-container">
               <a class="nav-item nav-link navbar-link" onClick={this.signIn}>Log in</a>
             </div>
-            <div class="navbar-link-container">
-              <a class="nav-item nav-link navbar-link mr-auto">Sign up</a>
-            </div>
           </div>
         </div>
       </nav>
     );
+
+
+    }
 
   }
 }
