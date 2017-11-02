ea964233507a7ec87cd285b194e8dc0a4208f977
Sayre Precure <shprecure@Sayres-MacBook-Pro.local>
2017-11-02 13:36:11 -0500

    loads of cleanup

--- src/App.js
+++ src/App.js
@@ -24,110 +24,95 @@ class GoogleLogin extends Component{
     };
   }
 
- /**
-  *  Initializes the API client library and sets up sign-in state
-  *  listeners.
-  */
+  // Initializes the API client library and sets up sign-in statelisteners.
   initClient() {
     var self = this;
     var obj = {};
-      // Client ID and API key from the Developer Console
+
+    // Client ID and API key from the Developer Console
     obj["apiKey"] = 'AIzaSyAWsOMuD2IvgMa_DtqEKJKmmG279bWOBpc';
     obj["clientId"] = '873484662570-llocoft6cvqr4rksup2j4a27cbu49df4.apps.googleusercontent.com';
+
     // Array of API discovery doc URLs for APIs used by the quickstart
     obj["discoveryDocs"] = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
     // Authorization scopes required by the API; multiple scopes can be
     // included, separated by spaces.
     obj["scope"] = 'https://www.googleapis.com/auth/drive.metadata.readonly';
-   gapi.client.init(obj).then(function () {
-     // Listen for sign-in state changes.
-     gapi.auth2.getAuthInstance().isSignedIn.listen(self.updateSigninStatus.bind(self));
 
-     // Handle the initial sign-in state.
-     self.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-   });
- }
+    gapi.client.init(obj).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(self.updateSigninStatus.bind(self));
 
- /**
-  *  On load, called to load the auth2 library and API client library.
-  */
- handleClientLoad() {
+      // Handle the initial sign-in state.
+      self.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+  }
+
+  // Loads the auth2 library and API client library.
+  handleClientLoad() {
    gapi.load('client:auth2', this.initClient.bind(this));
- }
+  }
 
- /**
-  *  Called when the signed in status changes, to update the UI
-  *  appropriately. After a sign-in, the API is called.
-  */
- updateSigninStatus(isSignedIn) {
-   if (isSignedIn) {
+  // Updates UI when sign in status changes. After a sign-in, the API is called.
+  updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
       this.setState({
         showAuth:'none',
         showSignout:'block'
       });
-   }
-   else {
-     this.setState({
-       showAuth:'block',
-       showSignout:'none'
-     });
-   }
- }
+    }
+    else {
+      this.setState({
+        showAuth:'block',
+        showSignout:'none'
+      });
+    }
+  }
 
- /**
-  *  Sign in the user upon button click.
-  */
- handleAuthClick(event) {
-   gapi.auth2.getAuthInstance().signIn();
- }
+  // Sign in the user upon button click.
+  handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+  }
 
- /**
-  *  Sign out the user upon button click.
-  */
- handleSignoutClick(event) {
-   gapi.auth2.getAuthInstance().signOut();
- }
+  // Sign out the user upon button click.
+  handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+  }
 
- /**
-  * Print an authorized user's filenames and ids.
-  */
- printFiles() {
-   gapi.client.drive.files.list({
-     'pageSize': 10,
-     'fields': "nextPageToken, files(id, name)"
-   }).then(function(response) {
-     var files = response.result.files;
-     if (files && files.length > 0) {
-       for (var i = 0; i < files.length; i++) {
-         var file = files[i];
-         console.log("Files:" + file.name + ' ID:' + file.id + ')');
-       }
-     } else {
-       console.log('No files found.');
-     }
-   });
- }
+  // Print an authorized user's filenames and ids.
+  printFiles() {
+    gapi.client.drive.files.list({
+      'pageSize': 10,
+      'fields': "nextPageToken, files(id, name)"
+    }).then(function(response) {
+      var files = response.result.files;
+      if (files && files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          console.log("Files:" + file.name + ' ID:' + file.id + ')');
+        }
+      } else {
+        console.log('No files found.');
+      }
+    });
+  }
 
-  render(){
+  render() {
     return (
       <div>
         {/*<!--Add buttons to initiate auth sequence and sign out-->*/}
-        <button
-        id="authorize-button"
-        style={{display: this.state.showAuth}}
-        onClick={this.handleAuthClick}>
-        Authorize
+        <button id="authorize-button" style={{display: this.state.showAuth}} onClick={this.handleAuthClick}>
+          Authorize
         </button>
-        <button
-        id="signout-button"
-        style={{display: this.state.showSignout}}
-        onClick={this.handleSignoutClick}>
-        Sign Out
+        <button id="signout-button" style={{display: this.state.showSignout}} onClick={this.handleSignoutClick}>
+          Sign Out
         </button>
         <pre id="content"></pre>
       </div>
     );
   }
+
   componentDidMount(){
     this.handleClientLoad();
   }
@@ -162,9 +147,11 @@ class demo extends Component{
     );
   }
 }
+
 //////////////////////////////////////////////////////////////////////////////
 // Main - You should only write components, functions, or routes here
 //////////////////////////////////////////////////////////////////////////////
+
 class App extends Component {
   constructor(props) {
     super(props);
