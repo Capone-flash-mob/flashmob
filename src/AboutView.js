f31bbe4676646a076d60ff54712e548c516b2c19
Sayre Precure <shprecure@Sayres-MacBook-Pro.local>
2017-11-07 00:43:00 -0600

    added AboutView file

---
+++ src/AboutView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Creates a page where users can view information about the application
+var AboutView = class AboutView extends React.Component {
+  render() {
+    return (
+      <div class="content">
+        <h1> This is the About Page </h1>
+      </div>
+    );
+  }
+}
+
+export default AboutView;
