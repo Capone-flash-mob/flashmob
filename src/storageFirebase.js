15b2e94d5fc253e98ebbd9992f0e63725239498b
Nicholas Gilpin <nicholascgilpin123@gmail.com>
2017-11-07 17:43:40 -0600

    Create file for storage service using components

--- src/storageFirebase.js
+++ src/storageFirebase.js
@@ -1,3 +1,10 @@
-/*
-Functions related to uploading/downloading/reading files from Firebase
-*/
+Functions and components that
+import firebase from 'firebase'
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
+function printRef(){
+  console.log("Firebase storage reference:");
+  console.log(storage);
+}
